ff10658615bf7fb5f8d47991c8c79170497b7652
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2021-11-03 19:22:38 +0530

    added cart view and functionality

--- models/cart.js
+++ models/cart.js
@@ -15,7 +15,9 @@ module.exports = class Cart {
                 cart = JSON.parse(fileContent);
             }
             console.log(cart);
-            const existingProductIndex = cart.products.findIndex((p) => p.id === id);
+            const existingProductIndex = cart.products.findIndex(
+                (p) => p.id === id
+            );
             const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
             if (existingProduct) {
@@ -29,7 +31,41 @@ module.exports = class Cart {
             cart.totalPrice = cart.totalPrice + productPrice;
             fs.writeFile(p, JSON.stringify(cart), (err) => {
                 console.log(err);
-            })
+            });
+        });
+    }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return;
+            }
+            const fetchedProducts = JSON.parse(fileContent);
+            const updatedCart = { ...fetchedProducts };
+            const product = updatedCart.products.find((p) => p.id === id);
+            if (!product) {
+                return;
+            }
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(
+                (p) => p.id !== id
+            );
+            updatedCart.totalPrice =
+                updatedCart.totalPrice - productPrice * productQty;
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if (err) {
+                cb(null);
+            } else {
+                cb(cart);
+            }
         });
     }
 };
--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const Cart = require("./cart");
+
 const p = path.join(
     path.dirname(process.mainModule.filename),
     "data",
@@ -18,19 +20,42 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
-        this.price = price;
+        this.price = +price;
     }
 
     async save() {
-        this.id = Math.random().toString(36).substr(2, 9);
         getProductsFromFile((products) => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
+            if (this.id) {
+                const existingProductIndex = products.findIndex(p => p.id === this.id);
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log(err);
+                });
+            }
+            else {
+                this.id = Math.random().toString(36).substr(2, 9);
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                });
+            }
+        });
+    }
+
+    static async deleteById(id) {
+        getProductsFromFile((products) => {
+            const product = products.find((p) => p.id === id);
+            const updatedProducts = products.filter((p) => p.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                if (!err) {
+                    Cart.deleteProduct(id, product.price);
+                }
             });
         });
     }
